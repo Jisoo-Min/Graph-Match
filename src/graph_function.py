dc0d2a43c13063e942f8ffaf59bd8ea1b31c8238
jiisoo <jsmin0415@gmail.com>
2019-10-30 15:35:10 -0700

    sadd functions for graphs

--- src/graph_function.py
+++ src/graph_function.py
@@ -1,3 +1,4 @@
+import pydotimport os
 import pydot
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -18,8 +19,9 @@ def is_subgraph(big, small):
         
         if(nx.is_isomorphic(sub_in_big, small) == True):
             print("It is a Subgraph!!")
-            break
-        
+            return True
+    
+    """
     # Draw two compared graphs
     fig = plt.figure(figsize=(5, 10))
     plt.subplot(2, 1, 1)
@@ -31,8 +33,11 @@ def is_subgraph(big, small):
     plt.title("smaller graph") 
     
     plt.show()
+    """
+    
+    return False
         
-	
+    
 ####################################################################
 # @unique_graph: unique sub-graph structures given the number of nodes
 # return unique_graph
@@ -73,7 +78,7 @@ def find_unique_subgraph(graphs, num_node):
 ####################################################################
 def read_subgraphs():
     sub_graphs = []
-    for id in range(10):
+    for id in range(27):
         sub_graph = nx.drawing.nx_pydot.read_dot("../data/subgraphs/index" + str(id) + ".dot")
         sub_graphs.append(sub_graph)
 
@@ -115,14 +120,42 @@ def contain_which_sub(graph):
     print(count_subs)
 
 
+####################################################################
+# Get all graphs. There are 280 graphs in total.
+####################################################################
+def read_all_graphs(path):
+    dot_files = os.listdir(path)
+    
+    graphs = []
+    for f in dot_files:
+        graphs.append(nx.drawing.nx_pydot.read_dot(path+ f))
+        
+    return graphs
+        
 
 
+####################################################################
+# Divide graphs into two groups.
+# One is the number of graphs with the subgraph and
+# the other is the number of graphs without the subgraph
+####################################################################
+def split_graphs_by(sub, graphs):
+    
+    include     = 0
+    not_include = 0
+    
+    for g in graphs:
+        if(is_subgraph(g, sub) == True):
+            include += 1
+        else:
+            not_include += 1
+            
+    return include, not_include
 
 def main():
+    """Test1: is_subgraph
     big_graph = nx.drawing.nx_pydot.read_dot("../data/basic_blocks/dot/Ap1Bigheights-CFG.dot")
     contain_which_sub(big_graph)
-
-    """
     small_graph = nx.MultiDiGraph()
     small_graph.add_node(0)
     small_graph.add_node(1)
@@ -132,9 +165,19 @@ def main():
 
     is_subgraph(big_graph, small_graph)
     """
+
+    """Test2: find_unique_subgraphs
     graphs = classify_graph.classify_graph("../data/basic_blocks/json/")
 
     find_unique_subgraph(graphs, 4) 
+
+    """
+
+    """Test3: split_graphs_by_subgraph
+    all_graphs = read_all_graphs("../data/basic_blocks/dot/")
+    subs = read_subgraphs()
+    split_graphs_by(subs[23], all_graphs)
+    """
     
 
 if __name__ == "__main__":
