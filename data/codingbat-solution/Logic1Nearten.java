a6ee2d29c6c3f2fbc2c13928b9a4ebbd22da5191
jiisoo <jsmin0415@gmail.com>
2019-11-20 00:33:32 -0800

    coding bat solutions

---
+++ data/codingbat-solution/Logic1Nearten.java
@@ -0,0 +1,8 @@
+class Logic1Nearten{ 
+/* Given a non-negative number "num", return true if num is within 2 of a 
+ * multiple of 10.
+ */
+public boolean nearTen(int num) {
+    return num % 10 <= 2 || num % 10 >= 8;
+}
+}
---
+++ data/codingbat-solution/Warmup1Lastdigit.java
@@ -0,0 +1,9 @@
+class Warmup1Lastdigit{ 
+/* Given two non-negative int values, return true if they have the same last 
+ * digit, such as with 27 and 57. Note that the % "mod" operator computes 
+ * remainders, so 17 % 10 is 7.
+ */
+public boolean lastDigit(int a, int b) {
+    return a % 10 == b % 10;
+}
+}
