5169f4849b9d63d49686ad1afcbf0134582f33a1
dosoung <48678405+dosoung@users.noreply.github.com>
2019-11-20 13:28:26 -0800

    update

--- data/codingbat-solution/Logic1Less20.java
+++ data/codingbat-solution/Logic1Less20.java
@@ -3,6 +3,7 @@ class Logic1Less20{
  * multiple of 20. So for example 38 and 39 return true, but 40 returns false.
  */
 public boolean less20(int n) {
-    return n % 20 == 19 || n % 20 == 18;
+    if((n % 20) == 19 || (n % 20) == 18) return 1;
+    else return 0;
 }
 }
