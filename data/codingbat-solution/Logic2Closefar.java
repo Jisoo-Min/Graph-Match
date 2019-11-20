80123509653e4619861f75116e266a6629cfbe74
Jigu <jsmin0415@gmail.com>
2019-11-20 13:49:40 -0800

    add if statements

--- data/codingbat-solution/Logic2Closefar.java
+++ data/codingbat-solution/Logic2Closefar.java
@@ -5,15 +5,27 @@ class Logic2Closefar{
  * value of a number.
  */
 public boolean closeFar(int a, int b, int c) {
-    return (isClose(a, b) && isFar(a, b, c)) ||
-        (isClose(a, c) && isFar(a, c, b));
+    if(isClose(a, b) && isFar(a, b, c)) || (isClose(a, c) && isFar(a, c, b)){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 public boolean isClose(int a, int b) {
-    return Math.abs(a - b) <= 1;
+    if(Math.abs(a - b) <= 1){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 public boolean isFar(int a, int b, int c) {
-    return Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2;
+    if(Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2){
+        return 1;
+    }else{
+        return 0;
+    }
+    
 }
 }
