0c0171267f850a187784aba09728d75702e8b89e
Jigu <jsmin0415@gmail.com>
2019-11-20 00:50:55 -0800

    add if statement

--- data/codingbat-solution/Array1Sum3.java
+++ data/codingbat-solution/Array1Sum3.java
@@ -2,6 +2,9 @@ class Array1Sum3{
 /* Given an array of ints length 3, return the sum of all the elements.
  */
 public int sum3(int[] nums) {
-    return nums[0] + nums[1] + nums[2];
+	int sum = 0;
+	for(int i = 0; i < 3; i++){
+		sum += nums[i];
+	}
 }
 }
