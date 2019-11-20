9f13d181cb85b83f09919edd5fb7c78e489e0805
dosoung <48678405+dosoung@users.noreply.github.com>
2019-11-20 13:34:30 -0800

    update

--- data/codingbat-solution/Logic1Lessby10.java
+++ data/codingbat-solution/Logic1Lessby10.java
@@ -3,7 +3,8 @@ class Logic1Lessby10{
  * than one of the others.
  */
 public boolean lessBy10(int a, int b, int c) {
-    return  Math.abs(a - b) >= 10 || Math.abs(a - c) >= 10 || 
-        Math.abs(b - c) >= 10;
+    if(Math.abs(a - b) >= 10 || Math.abs(a - c) >= 10 || 
+        Math.abs(b - c) >= 10) return 1;
+    else return 0;
 }
 }
