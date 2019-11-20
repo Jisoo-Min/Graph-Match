0a52005774af83fae39a7b3e1aadb62bbb912fe9
Jigu <jsmin0415@gmail.com>
2019-11-20 01:57:31 -0800

    codingbat solutions

--- data/codingbat-solution/Array1Sum3.java
+++ data/codingbat-solution/Array1Sum3.java
@@ -2,9 +2,6 @@ class Array1Sum3{
 /* Given an array of ints length 3, return the sum of all the elements.
  */
 public int sum3(int[] nums) {
-	int sum = 0;
-	for(int i = 0; i < 3; i++){
-		sum += nums[i];
-	}
+	return nums[0] + nums[1] + nums[2];
 }
 }
